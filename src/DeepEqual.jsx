9984d2c8645fec55d269484257c4bc44609aa3e5
Mohammad-Faisal <kiobostha300*#>
2021-05-16 05:55:00 +0600

    done

--- src/DeepEqual.jsx
+++ src/DeepEqual.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+var deepEqual = require('deep-equal')
 
 export const DeepEqual = () => {
 
+    const user1 = {
+        name: 'faisal'
+    }
 
-    return <div style={{margin:"10px",border:"1px solid" , }}>
-        The equality check result is
+    const user2 = {
+        name :'faisal'
+    }
+
+    //const isEqual = user1 === user2;
+
+    const deepEqualOrNot = deepEqual(user1 , user2);
+
+    return <div style={style}>
+        {deepEqualOrNot ? 'They are equal' :'They are not equal'}
     </div>
+}
+
+const style={
+    margin:"10px",
+    border:"1px solid"
 }
