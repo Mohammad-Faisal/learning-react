5fd3b2b86b99001183a5edea6bccd9bae46d2862
Mohammad-Faisal <kiobostha300*#>
2021-05-16 05:39:08 +0600

    first-commit

---
+++ src/DeepEqual.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DeepEqual = () => {
+
+
+    return <div style={{margin:"10px",border:"1px solid" , }}>
+        The equality check result is
+    </div>
+}
